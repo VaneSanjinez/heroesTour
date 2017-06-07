4782679212a4062fbbe80bf4195d84d54eec6151
Vanessa Sanjinez <vane.sanji@gmail.com>
2017-06-07 10:04:54 -0400

    First commi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+export class Hero {
+  id: number;
+  name: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  //template: `<h1>{{title}}</h1><h3>{{hero.name}} details</h3>`
+  template:
+  `<h1>{{title}}</h1>
+   <h2>{{hero.name}} details</h2>
+   <div><label> id: </label>{{hero.id}}</div>
+   <div>
+    <label> name: </label>
+    <input [(ngModel)] ="hero.name" placeholder= "name">
+   </div>
+  `
+
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heores';
+  hero : Hero ={
+    id: 1,
+    name: "windstorm"  
+};
 }
