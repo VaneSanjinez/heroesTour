018fd99439c4f2203d603b234e06d6805634ef60
Vanessa Sanjinez <vane.sanji@gmail.com>
2017-06-07 11:08:23 -0400

    Template html, ngFor for all the heroes, some CSS and able to modify name of heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,18 @@ export class Hero {
   id: number;
   name: string;
 }
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. A' },
+    { id: 12, name: 'Mr. B' },
+    { id: 13, name: 'Mr. C' },
+    { id: 14, name: 'Mr. D' },
+    { id: 15, name: 'Mr. E' },
+    { id: 16, name: 'Mr. F' },
+    { id: 17, name: 'Mr. G' },
+    { id: 18, name: 'Mr. H' },
+    { id: 19, name: 'Mr. I' },
+    { id: 20, name: 'Mr. J' }
+  ];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,19 +22,26 @@ export class Hero {
   //template: `<h1>{{title}}</h1><h3>{{hero.name}} details</h3>`
   template:
   `<h1>{{title}}</h1>
-   <h2>{{hero.name}} details</h2>
-   <div><label> id: </label>{{hero.id}}</div>
-   <div>
-    <label> name: </label>
-    <input [(ngModel)] ="hero.name" placeholder= "name">
-   </div>
+   <h2>The Heroes</h2>
+   <ul class = "heroes">
+    <li *ngFor = "let hero of heroes" (click)="onSelect(hero)">
+      <span class="badge"> {{hero.id}} </span>{{hero.name}}
+    </li>
+   </ul>
+   <h2>{{selectedHero.name}} details</h2>
+    <div><label>id: </label>{{selectedHero.id}}</div>
+    <div>
+    <label>name: </label>
+    <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+</div>
   `
-
 })
 export class AppComponent {
   title = 'Tour of Heores';
-  hero : Hero ={
-    id: 1,
-    name: "windstorm"  
-};
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
 }
+};
